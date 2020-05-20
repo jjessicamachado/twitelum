1299b31f91ba06c1a304fdca99a609d77ede1aa7
Jessica <jessicasmachado@live.com>
2020-05-19 23:05:10 -0300

    Primeiras configurações da caixinha de tweet

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
-import React, { Component, Fragment } from 'react';
-import Cabecalho from './components/Cabecalho'
-import NavMenu from './components/NavMenu'
-import Dashboard from './components/Dashboard'
-import Widget from './components/Widget'
-import TrendsArea from './components/TrendsArea'
-import Tweet from './components/Tweet'
+import React, { Component, Fragment } from "react";
+import Cabecalho from "./components/Cabecalho";
+import NavMenu from "./components/NavMenu";
+import Dashboard from "./components/Dashboard";
+import Widget from "./components/Widget";
+import TrendsArea from "./components/TrendsArea";
+import Tweet from "./components/Tweet";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      novoTweet: "",
+    };
+  }
+
   render() {
     return (
       <Fragment>
         <Cabecalho>
-            <NavMenu usuario="@omariosouto" />
+          <NavMenu usuario="@jessica" />
         </Cabecalho>
         <div className="container">
-            <Dashboard>
-                <Widget>
-                    <form className="novoTweet">
-                        <div className="novoTweet__editorArea">
-                            <span className="novoTweet__status">0/140</span>
-                            <textarea className="novoTweet__editor" placeholder="O que está acontecendo?"></textarea>
-                        </div>
-                        <button type="submit" className="novoTweet__envia">Tweetar</button>
-                    </form>
-                </Widget>
-                <Widget>
-                    <TrendsArea />
-                </Widget>
-            </Dashboard>
-            <Dashboard posicao="centro">
-                <Widget>
-                    <div className="tweetsArea">
-                        <Tweet />
-                    </div>
-                </Widget>
-            </Dashboard>
+          <Dashboard>
+            <Widget>
+              <form className="novoTweet">
+                <div className="novoTweet__editorArea">
+                  <span
+                    className={`novoTweet__status ${
+                      this.state.novoTweet.length > 140
+                        ? "novoTweet__status--invalido"
+                        : ""
+                    }`}
+                  >
+                    {this.state.novoTweet.length}/140
+                  </span>
+                  <textarea
+                    className="novoTweet__editor"
+                    value={this.state.novoTweet}
+                    onChange={(event) =>
+                      this.setState({ novoTweet: event.target.value })
+                    }
+                    placeholder="O que está acontecendo?"
+                  ></textarea>
+                </div>
+                <button
+                  type="submit"
+                  className="novoTweet__envia"
+                  disabled={
+                    this.state.novoTweet.length > 140 ||
+                    this.state.novoTweet.length === 0
+                  }
+                >
+                  Tweetar
+                </button>
+              </form>
+            </Widget>
+            <Widget>
+              <TrendsArea />
+            </Widget>
+          </Dashboard>
+          <Dashboard posicao="centro">
+            <Widget>
+              <div className="tweetsArea">
+                <Tweet />
+              </div>
+            </Widget>
+          </Dashboard>
         </div>
       </Fragment>
     );
