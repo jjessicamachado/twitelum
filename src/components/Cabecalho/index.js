d52cb1d7c4cc49e203934ca7b8947dcc15da56ce
Jessica <jessicasmachado@live.com>
2020-05-19 22:24:00 -0300

    Exercícios até o cap 6

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./cabecalho.css";
+
+class Cabecalho extends Component {
+  render() {
+    return (
+      <header className="cabecalho">
+        <div className="cabecalho__container container">
+          <h1 className="cabecalho__logo">
+            <a href="/">Twitelum</a>
+          </h1>
+          {this.props.children}
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Cabecalho;
---
+++ src/components/NavMenu/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import navMenuStyles from "./navMenu.module.css";
+
+class NavMenu extends Component {
+  render() {
+    return (
+      <nav className={navMenuStyles.navMenu}>
+        <ul className={navMenuStyles.navMenu__lista}>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/">
+              Bem vindo(a): <br />
+              <strong>{this.props.usuario}</strong>
+            </a>
+          </li>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/">
+              Página Inicial
+            </a>
+          </li>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/hashtags">
+              Hashtags
+            </a>
+          </li>
+          <li className={navMenuStyles.navMenu__item}>
+            <a className={navMenuStyles.navMenu__link} href="/logout">
+              Logout
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default NavMenu;
