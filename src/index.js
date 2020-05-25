8a66a93e9251e5397711c4008f82470a1f6000b5
Jessica <jessicasmachado@live.com>
2020-05-25 13:49:22 -0300

    Arquivos da última aula!

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ import "./assets/css/notificacao.css";
 import "./assets/css/novoTweet.css";
 // import './index.css';
 
-import App from "./App";
+import HomePage from "./pages/HomePage";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<HomePage />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from "react";
-import Cabecalho from "./components/Cabecalho";
-import NavMenu from "./components/NavMenu";
-import Dashboard from "./components/Dashboard";
-import Widget from "./components/Widget";
-import TrendsArea from "./components/TrendsArea";
-import Tweet from "./components/Tweet";
+import Cabecalho from "../../components/Cabecalho";
+import NavMenu from "../../components/NavMenu";
+import Dashboard from "../../components/Dashboard";
+import Widget from "../../components/Widget";
+import TrendsArea from "../../components/TrendsArea";
+import Tweet from "../../components/Tweet";
 
-class App extends Component {
+class HomePage extends Component {
   constructor() {
     super();
     this.state = {
@@ -89,4 +89,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default HomePage;
