adb7e3dcb5ddca584f2e4fd8807688938fd2c3a0
Jessica <jessicasmachado@live.com>
2020-05-25 21:32:01 -0300

    até o cap 19

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -2,15 +2,83 @@ import React, { Component, Fragment } from "react";
 import Cabecalho from "../../components/Cabecalho";
 import Widget from "../../components/Widget";
 import { NotificacaoContext } from "../../context/NotificacaoContext";
+import { LoginService } from "../../services/LoginService";
 
 import "./loginPage.css";
+
+const InputFormField = ({ id, label, errors, values, onChange }) => {
+  return (
+    <div className="loginPage__inputWrap">
+      <label className="loginPage__label" htmlFor={id}>
+        {label}
+      </label>
+
+      <input
+        className="loginPage__input"
+        type="text"
+        id={id}
+        name={id}
+        values={values[id]}
+        onChange={onChange}
+      />
+
+      <p style={{ color: "red" }}>{errors[id] && errors[id]}</p>
+    </div>
+  );
+};
 
 class LoginPage extends Component {
   static contextType = NotificacaoContext;
 
+  state = {
+    values: {
+      inputLogin: "",
+      inputSenha: "",
+    },
+    errors: {},
+  };
+
+  formValidations = () => {
+    const { inputLogin, inputSenha } = this.state.values;
+    const errors = {};
+
+    if (!inputLogin) {
+      errors.inputLogin = "Esse campo é obrigatório!";
+    }
+
+    if (!inputSenha) {
+      errors.inputSenha = "Esse campo é obrigatório!";
+    }
+
+    this.setState({ errors });
+  };
+
+  onFormFieldChange = ({ target }) => {
+    const value = target.value;
+    const name = target.name;
+    const values = { ...this.state.values, [name]: value };
+    this.setState({ values }, () => {
+      this.formValidations();
+    });
+  };
+
   fazerLogin = (infosDoEvento) => {
     infosDoEvento.preventDefault();
-    this.context.setMsg("Login Realizado com Sucesso!");
+
+    const dadosDeLogin = {
+      login: this.state.values.inputLogin,
+      senha: this.state.values.inputSenha,
+    };
+
+    LoginService.logar(dadosDeLogin)
+      .then(() => {
+        this.context.setMsg("Bem vindo ao Twitelum, login foi um sucesso!");
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        this.context.setMsg(`Erro ${err.status}` + err.message);
+        this.setState({ msgErro: err.message });
+      });
   };
 
   render() {
@@ -26,28 +94,21 @@ class LoginPage extends Component {
                 action="/"
                 onSubmit={this.fazerLogin}
               >
-                <div className="loginPage__inputWrap">
-                  <label className="loginPage__label" htmlFor="login">
-                    Login
-                  </label>
-                  <input
-                    className="loginPage__input"
-                    type="text"
-                    id="login"
-                    name="login"
-                  />
-                </div>
-                <div className="loginPage__inputWrap">
-                  <label className="loginPage__label" htmlFor="senha">
-                    Senha
-                  </label>
-                  <input
-                    className="loginPage__input"
-                    type="password"
-                    id="senha"
-                    name="senha"
-                  />
-                </div>
+                <InputFormField
+                  id="inputLogin"
+                  label="Login:"
+                  onChange={this.onFormFieldChange}
+                  values={this.state.values}
+                  errors={this.state.errors}
+                />
+                <InputFormField
+                  id="inputSenha"
+                  label="Senha:"
+                  onChange={this.onFormFieldChange}
+                  values={this.state.values}
+                  errors={this.state.errors}
+                  type="password"
+                />
                 {/* <div className="loginPage__errorBox">
                                     Mensagem de erro!
                                 </div> */}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+class PrivateRoute extends React.Component {
+  estaAutenticado = () => {
+    if (localStorage.getItem("TOKEN")) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  render() {
+    const { component: Component, ...props } = this.props;
+
+    if (this.estaAutenticado()) {
+      return <Component {...props} />;
+    } else {
+      return <Redirect to="/login" />;
+    }
+  }
+}
+
 class Roteamento extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={HomePage} exact />
+        <PrivateRoute path="/" component={HomePage} exact />
         <Route path="/login" component={LoginPage} />
+        <Route component={NotFoundPage} />
       </Switch>
     );
   }
