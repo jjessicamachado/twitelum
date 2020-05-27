2b7a2d61af48cbafe32f2dc3d31a03151904d0a6
Jessica <jessicasmachado@live.com>
2020-05-26 22:00:50 -0300

    até o cap 28

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -5,6 +5,7 @@ import Dashboard from "../../components/Dashboard";
 import Widget from "../../components/Widget";
 import TrendsArea from "../../components/TrendsArea";
 import Tweet from "../../components/Tweet";
+import { Modal } from "../../components/Modal";
 
 class HomePage extends Component {
   constructor() {
@@ -12,18 +13,85 @@ class HomePage extends Component {
     this.state = {
       novoTweet: "",
       tweets: [],
+      tweetAtivoNoModal: {},
     };
+  }
+
+  componentDidMount() {
+    fetch(
+      `https://twitelum-api.herokuapp.com/tweets?X-AUTH-TOKEN=${localStorage.getItem(
+        "TOKEN"
+      )}`
+    )
+      .then((response) => response.json())
+      .then((tweets) => {
+        this.setState({
+          tweets,
+        });
+      });
   }
 
   adicionaTweet = (infosDoEvento) => {
     infosDoEvento.preventDefault();
     if (this.state.novoTweet.length > 0) {
-      this.setState({
-        tweets: [this.state.novoTweet, ...this.state.tweets],
-        novoTweet: "",
-      });
+      fetch(
+        `https://twitelum-api.herokuapp.com/tweets?X-AUTH-TOKEN=${localStorage.getItem(
+          "TOKEN"
+        )}`,
+        {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify({ conteudo: this.state.novoTweet }),
+        }
+      )
+        .then((respostaDoServer) => {
+          return respostaDoServer.json();
+        })
+        .then((tweetVindoDoServidor) => {
+          console.log(tweetVindoDoServidor);
+          this.setState({
+            novoTweet: "",
+            tweets: [tweetVindoDoServidor, ...this.state.tweets],
+          });
+        });
     }
   };
+
+  removeTweet(idTweetQueVaiSerRemovido) {
+    console.log(idTweetQueVaiSerRemovido);
+    fetch(
+      `https://twitelum-api.herokuapp.com/tweets/${idTweetQueVaiSerRemovido}?X-AUTH-TOKEN=${localStorage.getItem(
+        "TOKEN"
+      )}`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((data) => data.json())
+      .then((response) => {
+        console.log(response);
+        const listaDeTweetsAtualizada = this.state.tweets.filter(
+          (tweet) => tweet._id !== idTweetQueVaiSerRemovido
+        );
+        this.setState({
+          tweets: listaDeTweetsAtualizada,
+        });
+        this.fechaModal();
+      });
+  }
+
+  abreModal = (tweetQueVaiProModal) => {
+    this.setState(
+      {
+        tweetAtivoNoModal: tweetQueVaiProModal,
+      },
+      () => {
+        console.log(this.state.tweetAtivoNoModal);
+      }
+    );
+  };
+
+  fechaModal = () => this.setState({ tweetAtivoNoModal: {} });
 
   render() {
     return (
@@ -77,13 +145,51 @@ class HomePage extends Component {
                   <div>Faça um tweet</div>
                 ) : (
                   this.state.tweets.map((tweetInfo, index) => {
-                    return <Tweet key={tweetInfo + index} texto={tweetInfo} />;
+                    return (
+                      <Tweet
+                        key={tweetInfo._id}
+                        id={tweetInfo._id}
+                        texto={tweetInfo.conteudo}
+                        usuario={tweetInfo.usuario}
+                        likeado={tweetInfo.likeado}
+                        totalLikes={tweetInfo.totalLikes}
+                        removivel={tweetInfo.removivel}
+                        onClickNaAreaDeConteudo={() =>
+                          this.abreModal(tweetInfo)
+                        }
+                        removeHandler={(event) =>
+                          this.removeTweet(tweetInfo._id)
+                        }
+                      />
+                    );
                   })
                 )}
               </div>
             </Widget>
           </Dashboard>
         </div>
+        <Modal
+          isAberto={Boolean(this.state.tweetAtivoNoModal._id)}
+          onFechando={this.fechaModal}
+        >
+          {() => (
+            <Tweet
+              key={this.state.tweetAtivoNoModal._id}
+              id={this.state.tweetAtivoNoModal._id}
+              texto={this.state.tweetAtivoNoModal.conteudo}
+              usuario={this.state.tweetAtivoNoModal.usuario}
+              likeado={this.state.tweetAtivoNoModal.likeado}
+              totalLikes={this.state.tweetAtivoNoModal.totalLikes}
+              removivel={this.state.tweetAtivoNoModal.removivel}
+              onClickNaAreaDeConteudo={() =>
+                this.abreModal(this.state.tweetAtivoNoModal)
+              }
+              removeHandler={(event) =>
+                this.removeTweet(this.state.tweetAtivoNoModal._id)
+              }
+            />
+          )}
+        </Modal>
       </Fragment>
     );
   }
